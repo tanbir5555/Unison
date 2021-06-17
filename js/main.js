70dd84df289f322580a8bea284634e2649209805
tanbir5555 <tanbirhawlader12690@gmail.com>
2021-06-18 00:34:46 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function s(ele) {
+  return document.querySelector(ele);
+}
+function sAll(ele) {
+  return document.querySelectorAll(ele);
+}
+
+let closeBtn = s('#closeMenu');
+let openBtn = s('#openMenu');
+
+let menu = s('#menu');
+menuOpen = false;
+openBtn.addEventListener('click', function () {
+    callMenu();
+});
+
+closeBtn.addEventListener('click', function () {
+    callMenu();
+});
+
+
+function callMenu(){
+    if (menuOpen) {
+        menuOpen = false;
+        menu.style.left = '-400px';
+      } else {
+        menuOpen = true;
+        menu.style.left = '0px';
+      }
+}
